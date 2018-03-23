b111692fc126cc42a0acf8c20430187899beb781
Kotavy <tavius.koktavy@gmail.com>
2018-03-24 03:42:21 +0400

    Auth

--- python_scripts/add_events.py
+++ python_scripts/add_events.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from apiclient.discovery import build
+import apiclient
 from httplib2 import http
 from oauth2client import file, client, tools
 
@@ -9,7 +9,7 @@ creds = store.get()
 if not creds or creds.invalid:
     flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
     creds = tools.run_flow(flow, store)
-GCAL = discovery.build('calendar', 'v3', http=creds.authorize(Http()))
+GCAL = apiclient.discovery.build('calendar', 'v3', http=creds.authorize(Http()))
 
 #for each event in JSON:
 event = {
@@ -27,5 +27,5 @@ event = {
 }
 
 event = GCAL.events().insert(calendarId='nyuadeventful@gmail.com', body=event).execute()
-print 'Event created: %s' % (event.get('htmlLink'))
+#print 'Event created: %s' % (event.get('htmlLink'))
 #}
