81d049050f16f61f396954feb2d7558a435cfe97
Kotavy <tavius.koktavy@gmail.com>
2018-03-24 01:50:45 +0400

    Test heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bp = require('body-parser')
 const py = require('python-shell')
-const selenium = new py('python_scripts/selenium_script.py');
+//const selenium = new py('python_scripts/selenium_script.py');
 const pug = require('pug')
 const fs = require('fs')
 const path = require('path')
@@ -21,7 +21,7 @@ express()
 
     //.get('/api/*', (req, res) => res.send("Woo!"))
     .get('/', jsonLoad)
-    .get('/python', scrape)
+    //.get('/python', scrape)
 
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
@@ -39,56 +39,56 @@ function jsonLoad(req, res) {
     })
 }
 
-// Run python to scrape OrgSync for events, save to data folder:
-function scrape(req, res) {
-    // var username;
-    // var password;
-    // prompt('Whats your name?', function (input) {
-    //     console.log(input);
-    //     process.exit();
-    // });
-
-
-    // prompt.start();
-    // prompt.get(['username', 'email'], function (err, result) {
-    //     username = result.username;
-    //     password = result.password;
-    // });
-    // console.log(username);
-
-
-    // if (person == null || person == "") {
-    //     res.status(404).send('No name sent. It\'s ok, you don\'t have to trust us.')
-    // } else {
-    //     var shell = new py('script.py', { mode: 'text '});
-    //     shell.send('hello world!');
-    // }
-
-    // console.log("Calling selenium")
-    // selenium.on('message', function (message) {
-    //   // received a message sent from the Python script (a simple "print" statement)
-    //   console.log(message);
-
-
-
-    // py.run('python_scripts/selenium_script.py', function (err) {
-    //   if (err) {
-    //         console.log(err)
-    //         res.status(404).send('Watch out for this Bad Request!')
-    //         console.log("End of error, fool")
-    //   }
-    //   py.end(jsonLoad);
-    // })
-}
-
-function prompt(question, callback) {
-    var stdin = process.stdin,
-        stdout = process.stdout;
-
-    stdin.resume();
-    stdout.write(question);
-
-    stdin.once('data', function (data) {
-        callback(data.toString().trim());
-    });
-}
+// // Run python to scrape OrgSync for events, save to data folder:
+// function scrape(req, res) {
+//     // var username;
+//     // var password;
+//     // prompt('Whats your name?', function (input) {
+//     //     console.log(input);
+//     //     process.exit();
+//     // });
+//
+//
+//     // prompt.start();
+//     // prompt.get(['username', 'email'], function (err, result) {
+//     //     username = result.username;
+//     //     password = result.password;
+//     // });
+//     // console.log(username);
+//
+//
+//     // if (person == null || person == "") {
+//     //     res.status(404).send('No name sent. It\'s ok, you don\'t have to trust us.')
+//     // } else {
+//     //     var shell = new py('script.py', { mode: 'text '});
+//     //     shell.send('hello world!');
+//     // }
+//
+//     // console.log("Calling selenium")
+//     // selenium.on('message', function (message) {
+//     //   // received a message sent from the Python script (a simple "print" statement)
+//     //   console.log(message);
+//
+//
+//
+//     // py.run('python_scripts/selenium_script.py', function (err) {
+//     //   if (err) {
+//     //         console.log(err)
+//     //         res.status(404).send('Watch out for this Bad Request!')
+//     //         console.log("End of error, fool")
+//     //   }
+//     //   py.end(jsonLoad);
+//     // })
+// }
+//
+// function prompt(question, callback) {
+//     var stdin = process.stdin,
+//         stdout = process.stdout;
+//
+//     stdin.resume();
+//     stdout.write(question);
+//
+//     stdin.once('data', function (data) {
+//         callback(data.toString().trim());
+//     });
+// }
