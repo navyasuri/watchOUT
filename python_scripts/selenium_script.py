dc198da564e1859bca679674c83f658bb8291c10
oscargomezq <oscar.gomezq@hotmail.com>
2018-03-23 21:53:25 +0400

    Add files via upload

---
+++ python_scripts/selenium_script.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import selenium
+import codecs
+import getpass
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+# Set login info
+myusername=input("username: ")
+mypassword=getpass.getpass("password: ")
+
+# Open browser
+browser = webdriver.Chrome()
+browser.get('https://students.nyuad.nyu.edu')
+
+# Login using selenium
+username = browser.find_element_by_name("j_username")
+password = browser.find_element_by_name("j_password")
+username.send_keys(myusername)
+password.send_keys(mypassword)
+browser.find_element_by_name("_eventId_proceed").click()
+
+time.sleep(8)
+
+# Now in the MFA authentication page
+html = browser.page_source
+soup = BeautifulSoup(html, 'lxml')
+iframe = browser.find_element_by_tag_name("iframe")
+ac = ActionChains(browser)
+ac.move_to_element_with_offset(iframe, 410, 60).click().perform()
+
+time.sleep(8)
+
+# Go to calendar page
+browser.get('https://students.nyuad.nyu.edu/calendars/')
+
+time.sleep(5)
